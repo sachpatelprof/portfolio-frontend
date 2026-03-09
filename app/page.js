e0fad053d88821f04f87b75605055ef5ac2f462f
SACH PATEL <sachupatel774@gmail.com>
2026-03-10 02:43:45 +0530

    speen insights

--- app/page.js
+++ app/page.js
@@ -2,6 +2,8 @@ import Navbar from "@/components/Navbar/Navbar"
 import Hero from "@/components/Hero/Hero"
 import Footer from "@/components/Footer/Footer"
 
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 export default function Home() {
   return (
     <>
